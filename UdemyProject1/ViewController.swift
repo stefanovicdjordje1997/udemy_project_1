caf6990ac867531261687dbac8c873fc7134a6bd
Djordje <stefanovic.djordje1997@gmail.com>
2024-02-29 18:33:10 +0100

    Initial Commit

---
+++ UdemyProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UdemyProject1
+//
+//  Created by Djordje Stefanovic on 29.2.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UdemyProject1UITests/UdemyProject1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UdemyProject1UITestsLaunchTests.swift
+//  UdemyProject1UITests
+//
+//  Created by Djordje Stefanovic on 29.2.24..
+//
+
+import XCTest
+
+final class UdemyProject1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
