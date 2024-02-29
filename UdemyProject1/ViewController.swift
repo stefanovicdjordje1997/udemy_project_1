b240eebc8203f6f2f8f766228812334cf555ee4e
Djordje <stefanovic.djordje1997@gmail.com>
2024-02-29 19:21:03 +0100

    Made tableView with cities and handle tap on cell

--- UdemyProject1/ViewController.swift
+++ UdemyProject1/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    var cities: [String: String] = ["Belgrade": "Serbia", "Amsterdam": "Netherlands", "Budapest": "Hungary", "Paris": "France", "Minhen": "Germany", "Bejing": "China", "Moscow": "Russia"]
+    
+    var cityNames: [String] = []
+    let cellIdentifier = "cell"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cityNames = Array(cities.keys)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
     }
+    
 
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        var cellContentConfiguration = cell.defaultContentConfiguration()
+        let cityName = cityNames[indexPath.row]
+        cellContentConfiguration.text = cityName
+        cellContentConfiguration.secondaryText = cities[cityName]
+        cell.contentConfiguration = cellContentConfiguration
+        return cell
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cityName = cityNames[indexPath.row]
+        print("City: \(cityName)\t Country: \(cities[cityName] ?? "")")
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
